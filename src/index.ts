2221487a465c896054ea9a7519b5483ef1a57ef7
Felinto <emersong20.email@gmail.com>
2022-08-05 23:35:04 +0000

    remove unsued depedencies

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
 import { Bot } from "grammy";
 import "dotenv/config";
 import { Context } from "./types/context";
-import { hydrateFiles } from "@grammyjs/files";
-import { useFluent } from "@grammyjs/fluent";
-import { Fluent } from "@moebius/fluent";
-import { apiThrottler } from "@grammyjs/transformer-throttler";
 import { handleBot } from "./handle";
-
-const fluent = new Fluent();
 
 if (process.env.BANNED_WORDS?.split(",").length === 0) {
   throw new Error("BANNED_WORDS is required");
@@ -15,9 +9,5 @@ if (process.env.BANNED_WORDS?.split(",").length === 0) {
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const bot = new Bot<Context>(BOT_TOKEN);
-bot.use(useFluent({ fluent, defaultLocale: "pt_br" }));
-bot.api.config.use(hydrateFiles(BOT_TOKEN));
-const throttler = apiThrottler();
-bot.api.config.use(throttler);
 bot.use(handleBot);
 bot.start();
